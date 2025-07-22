1c99f3c3ba36d7a2d336c3a852b24a22e39996e7
Esmira Aghamedova <esmo.agamedova2020@gmail.com>
2025-07-22 14:36:51 +0400

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,11 @@
+function createUserManager(id, name) {
+    let userId = id;
+    let userName = name;
+
+    function addUser(user) {
+        let user = {userId, userName};
+        return user;
+    }
+
+
+}
